a30665c3a7ef7ccfe8a24eed52943eda030c53e8
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-25 11:25:36 +0600

    designed login page

--- src/Home/Login.jsx
+++ src/Home/Login.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className='flex justify-center items-center h-[100vh]'>
-            <form className="card-bod w-11/12 max-w-[300px]">
-        <div className="form-control">
+        <div className='flex justify-center items-center h-[100vh] gap-32'>
+                      <img src='/logos/checkit.png' className='w-[300px]'/>
+          <div className='w-11/12 max-w-[350px]'>
+          <form className="">
+            <h1 className='text-sm font-bold text-end'>Log in with</h1>
+
+            <div className='grid grid-cols-3 gap-1 mt-6'>
+
+                 <div className='flex items-center text-sm gap-1 justify-center border p-3 border-gray-400 hover:outline hover:outline-gray-200' role='button'>
+                  <h1>Google</h1>
+                 <FaGoogle />
+                 </div>
+                 <div className='flex items-center text-sm gap-1 justify-center border p-3 border-gray-400  hover:outline hover:outline-gray-200'>
+                  <h1>Facebook</h1>
+                  <FaFacebookF />
+                 </div>
+                 <div className='flex items-center text-sm gap-1 justify-center border p-3 border-gray-400  hover:outline hover:outline-gray-200'>
+                  <h1>Github</h1>
+                  <FaGithub/>
+                 </div>
+    
+            </div>
+            <div className='flex items-center gap-3 mt-2'>
+            <hr class=" border border-black w-full" />
+            <p>or</p>
+            <hr class=" border border-black w-full" />
+            </div>
+        <div className="form-control mt-1">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
@@ -15,14 +44,15 @@ const Login = () => {
             <span className="label-text">Password</span>
           </label>
           <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn text-white bg-gradient-to-r from-emerald-400 to-cyan-400 bg-cyan-500  hover:bg-gradient-to-r hover:from-emerald-500 hover:to-cyan-500 hover:bg-cyan-500 ">Login</button>
         </div>
       </form>
+      <button className='btn w-full mt-4'>Try as guest</button>
+      <p className='mt-3 text-center text-sm'>Don't have an account? <Link className='underline font-bold' to='/signup'>Create new account</Link></p>
+          </div>
         </div>
     );
 };
