763cd34335f09fa579fcaff42fc2a0728cfad741
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-25 15:56:09 +0600

    added today page

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div className='relative'>
-            <div className='absolute right-5 top-5'>
-                <img src='/logos/checkit.png' className='w-[40px] h-[40px]'/>
-            </div>
-        <div className="drawer lg:drawer-open w-1/2 flex gap-10 ">
+        <div className=''>
+         
+        <div className="drawer lg:drawer-open flex ">
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
   {/* <div className="drawer-content flex flex-col items-center justify-center">
 
@@ -15,15 +14,24 @@ const Dashboard = () => {
       Open drawer
     </label>
   </div> */}
-  <div className="drawer-side">
+  <div className="w-1/4 h-[100vh] ">
     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="menu bg-base-200 text-base-content p-4 ">
+    <ul className="menu gap-4  p-4 w-full h-full text-white bg-gradient-to-r from-emerald-400 to-cyan-400 bg-cyan-500">
       {/* Sidebar content here */}
-      <li><a>Profile</a></li>
+        <div className='bg-white flex justify-between rounded-lg relative w-full'>
+        <input className='p-2 outline-0 rounded-lg w-full' placeholder='search '/>
+        <FaSearch className='absolute top-3 right-2'/>
+        </div>
+      <li><Link to='/app/today'>Today</Link></li>
       <li><a>Sidebar Item 2</a></li>
     </ul>
   </div>
+  <div className='w-3/4'>
+  <nav className='flex justify-end px-5 py-3'>
+                <img src='/logos/checkit.png' className='w-[40px] h-[40px]'/>
+            </nav>
   <Outlet/>
+  </div>
 </div>
 
         </div>
--- src/Dashboard/Today.jsx
+++ src/Dashboard/Today.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { IoIosAdd, IoIosAddCircle } from "react-icons/io";
 
 const Today = () => {
+
+    const [today,setToday] = useState()
+    const [presentTime,setPresentTime] = useState()
+    const [icon,setIcon] = useState(false)
+
+    const mouseIn = ()=>{
+        setIcon(true)
+    }
+    const mouseOut = ()=>{
+        setIcon(false)
+    }
+
+    useEffect(()=>{
+        setInterval(()=>{
+            const date = new Date()
+            setToday(date.toLocaleDateString())
+            setPresentTime(date.toLocaleTimeString())
+        },1000)
+    },[])
+
     return (
-        <div>
-            Today
+        <div className='w-10/12 mx-auto'>
+            <div className="flex mt-5 border-b pb-3 border-black justify-between">
+            <div>
+            <h1 className=''>{today}</h1>
+            <h1 className='font-sans text-xl font-bold'>{presentTime}</h1>
+            </div>
+            <h1 className='text-3xl font-bold '>Today</h1>
+            </div>
+            <div className='mt-5'>
+                <button className="flex items-center gap-2 " onMouseEnter={mouseIn} onMouseLeave={mouseOut}>{icon ?<IoIosAddCircle  className="text-green-500 text-3xl"/> : <IoIosAdd  className="text-green-500 text-3xl"/>} Add Task</button>
+            </div>
         </div>
     );
 };
