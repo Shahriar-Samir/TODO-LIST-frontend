3522ced73645821c3c27315a7cd67c97e4fe1fee
Shahriar-Samir <shabusiness035@gmail.com>
2024-06-25 10:02:21 +0600

    added login page

--- src/Home/Login.jsx
+++ src/Home/Login.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            Login
+        <div className='flex justify-center items-center h-[100vh]'>
+            <form className="card-bod w-11/12 max-w-[300px]">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
         </div>
     );
 };
